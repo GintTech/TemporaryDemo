f4894b9abbfe02b03bf66b18a82c07a494b89e25
James Ginther <james.ginther01@gmail.com>
2018-09-06 11:36:33 -0600

    Created backend and connection string

---
+++ src/Backend/BLL/StudentController.cs
@@ -0,0 +1,21 @@
+﻿using Backend.DAL;
+using Backend.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Backend.BLL
+{
+    class StudentController // controls access
+    {
+        public List<Student> ListAllStudents()
+        {
+            using (var context = new MyDatabaseContext())
+            {
+                return context.Students.ToList();
+            }
+        }
+    }
+}
