d50268a7c637a620238ba080e9d19ad1deb58682
James Ginther <james.ginther01@gmail.com>
2018-09-06 11:52:54 -0600

    Added gridview to about

--- src/Backend/BLL/StudentController.cs
+++ src/Backend/BLL/StudentController.cs
@@ -2,14 +2,17 @@
 using Backend.Entities;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel; //for [DataObject]
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Backend.BLL
 {
-    class StudentController // controls access
+    [DataObject]
+    public class StudentController // controls access
     {
+        [DataObjectMethod(DataObjectMethodType.Select)]
         public List<Student> ListAllStudents()
         {
             using (var context = new MyDatabaseContext())
